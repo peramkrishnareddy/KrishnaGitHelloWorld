a42ad9e547197f5474f883c3bc031465a50ba386
peramkrishnareddy <59649996+peramkrishnareddy@users.noreply.github.com>
2020-01-26 19:54:33 +0530

    Create HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,8 @@
+class HelloWorld2 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World2" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World2 from krishna"); 
+    } 
