5c99f3b672940fbfc2491095cadb0bc23b59c71c
peramkrishnareddy <59649996+peramkrishnareddy@users.noreply.github.com>
2020-01-26 20:10:46 +0530

    update 1.0

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,8 +1,8 @@
-class HelloWorld2 
+class HelloWorld
 { 
     // Your program begins with a call to main(). 
-    // Prints "Hello, World2" to the terminal window. 
+    // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World2 from krishna"); 
+        System.out.println("Hello, World from krishna"); 
     } 
